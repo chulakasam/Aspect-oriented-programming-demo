8bcf0414649e7de90e1af7719d7b25c8d7c2f79a
chulakasam <sampathchulaka33872@gmail.com>
2025-05-02 11:55:12 +0530

    spring aop with @before advice reuse pointcut

--- point-cut-declaration/src/main/java/lk/ijse/pointcutdeclaration/PointCutDeclarationApplication.java
+++ point-cut-declaration/src/main/java/lk/ijse/pointcutdeclaration/PointCutDeclarationApplication.java
@@ -1,13 +1,33 @@
 package lk.ijse.pointcutdeclaration;
 
+import lk.ijse.pointcutdeclaration.dao.AccountDAO;
+import lk.ijse.pointcutdeclaration.dao.MemberShipDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PointCutDeclarationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PointCutDeclarationApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MemberShipDAO memberShipDAO) {
+        return runner -> {
+            demoBeforeAdvice(accountDAO,memberShipDAO);
+        };
+    }
+    private void demoBeforeAdvice(AccountDAO accountDAO,MemberShipDAO memberShipDAO) {
+        // call business method
+        Account myAccount = new Account();
+        accountDAO.addAccount(myAccount,true);
+        accountDAO.doWork();
+
+        memberShipDAO.addAccount();
+        memberShipDAO.goToSleep();
     }
 
 }
